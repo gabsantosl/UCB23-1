7d0ec30119767b4097cf4cca2f504f7d2d2f9f7e
Gabriela Santos <82168937+gabsantosl@users.noreply.github.com>
2023-04-04 11:30:06 -0300

    Add files via upload

---
+++ 04-04/dafaw.c
@@ -0,0 +1,29 @@
+int main()
+
+{
+
+int p,q;
+
+p=1;
+
+q=1;
+
+printf("Valor de p = %d\n", p);
+
+printf("Valor de q = %d\n", q);
+
+printf("\n Criando o Incremento: p++\n");
+
+p++;
+
+printf("Criando um Decremento: q--\n");
+
+q--;
+
+printf("\n O novo valor para p = %d\n", p);
+
+printf(" O novo valor para q = %d\n",q);
+
+return 0;
+
+}
