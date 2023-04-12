a3f91393a9f4cbc8dc61319c93f09e39d6453203
Gabriela Santos <gabsantosleite@gmail.com>
2023-04-11 23:06:37 -0300

    q3

--- Programação Estruturada/Avaliações/Lista 1/L1_Q3_GABRIELA_SANTOS.cpp
+++ Programação Estruturada/Avaliações/Lista 1/L1_Q3_GABRIELA_SANTOS.cpp
@@ -2,7 +2,13 @@
 GABRIELA DOS SANTOS LEITE UC22100365	
 LISTA DE EXERCÍCIOS 1
 
-Q3- 
+Q3- Elabore um programa que faça o sorteio de vários números e ao final 
+    mostra: 
+    a) A quantidade de números sorteados; 
+    b) O Maior numero sorteado; 
+    c) Quantos números pares foram sorteados; 
+    Observação: O programa deve finalizar quando for sorteado o valor 0 
+    (zero)e utilizar no máximo 3 variáveis não utilizar vetores ou matrizes. 
 */
 
 #include <stdio.h>
