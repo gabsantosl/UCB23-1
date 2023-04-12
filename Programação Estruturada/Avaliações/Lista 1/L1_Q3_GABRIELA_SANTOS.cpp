df7f848d8e7bf0896f9e80db8dda6055ad7b9a77
Gabriela Santos <gabsantosleite@gmail.com>
2023-04-11 22:03:38 -0300

    q3

---
+++ Programação Estruturada/Avaliações/Lista 1/L1_Q3_GABRIELA_SANTOS.cpp
@@ -0,0 +1,17 @@
+/* 
+GABRIELA DOS SANTOS LEITE UC22100365	
+LISTA DE EXERCÍCIOS 1
+
+Q3- 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "Portuguese");
+
+	return 0;
+}
