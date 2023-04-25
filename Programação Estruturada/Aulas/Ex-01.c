5c7721997bb88fa646d8a432f6532d4848a2b16c
Gabriela Santos <82168937+gabsantosl@users.noreply.github.com>
2023-04-25 09:35:06 -0300

    Add files via upload

---
+++ Programação Estruturada/Aulas/Ex-01.c
@@ -0,0 +1,19 @@
+/*
+GABRIELA DOS SANTOS LEITE UC22100365
+EXERCÍCIOS 25/04/23
+
+EX-01) 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	
+	
+	return 0;
+}
