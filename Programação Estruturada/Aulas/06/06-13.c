bdfb30e8d5ad3f594ee75fef9fd337cb88f0b95e
Gabriela Santos <gabsantosleite@gmail.com>
2023-06-15 19:26:45 -0300

    aye

---
+++ Programação Estruturada/Aulas/06/06-13.c
@@ -0,0 +1,23 @@
+/*
+GABRIELA DOS SANTOS LEITE UC22100365
+AULA INVERTIDA:
+
+EX-01) Deverá fazer um estudo sobre os temas abaixo e gravar um vídeo 
+       explicativo sobre o código criado.
+       OBS.: Pode ser um único vídeo contendo os quatro exemplos.
+
+    TEMAS: malloc, calloc, realloc, free.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "Portuguese");
+
+    
+
+return 0;
+}
